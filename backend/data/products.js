f934c6087e9630aecbf5ecd182a1e898909d2587
priya <priyamtha1509@gmail.com>
2025-05-15 17:19:43 +0530

    done

--- backend/data/products.js
+++
@@ -1,7 +0,0 @@
-const products = [
-  { id: 1, name: 'Gold Ring', price: 5000 },
-  { id: 2, name: 'Gold Necklace', price: 15000 },
-  { id: 3, name: 'Gold Bracelet', price: 8000 },
-];
-
-module.exports = products;
