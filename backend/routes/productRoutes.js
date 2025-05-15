dcfcab765b7f9dc1a1ee2b1a5ce7aeb2e52019eb
priya <priyamtha1509@gmail.com>
2025-05-15 16:56:19 +0530

    done

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
+const { updateProduct } = require('../controllers/productController');
 // Import both functions from productController
 const { getProducts, addProduct } = require('../controllers/productController');
 
 router.get('/', getProducts);
 router.post('/', addProduct);
+router.put('/:id', updateProduct);
 
 module.exports = router;
