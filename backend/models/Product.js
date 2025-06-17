141b963bfdad8c7d54daa8082e48f7a3aa32436c
priya <priyamtha1509@gmail.com>
2025-06-17 12:13:48 +0530

    Implement relationship between User and Product models

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
   price: { type: Number, required: true, min: 0 },
+   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } 
 });
 
 const Product = mongoose.model('Product', productSchema);
